d0045a946842aaa5e9c8c69688cb8a9d5646e928
Shrish Raj Gupta <97391054+ShrishRajGupta@users.noreply.github.com>
2024-11-13 11:20:20 +0530

    Create a.cpp

---
+++ code/a.cpp
@@ -0,0 +1,98 @@
+/*  श्रीगुरु चरन सरोज रज   निज मनु मुकुरु सुधारि ।   बरनउँ रघुबर बिमल जसु
+    जो दायकु फल चारि ॥   बुद्धिहीन तनु जानिके   सुमिरौं पवन-कुमार ।
+    बल बुधि बिद्या देहु मोहिं  हरहु कलेस बिकार ॥
+
+॥ चौपाई ॥ जय हनुमान ज्ञान गुन सागर । जय कपीस तिहुँ लोक उजागर ॥    राम दूत अतुलित बल धामा ।  अंजनि पुत्र पवनसुत नामा ॥  महाबीर बिक्रम बजरंगी ।    कुमति निवार सुमति के संगी ॥
+    कंचन बरन बिराज सुबेसा ।   कानन कुण्डल कुँचित केसा ॥४ हाथ बज्र अरु ध्वजा बिराजै ।  काँधे मूँज जनेउ साजै ॥    शंकर स्वयं/सुवन केसरी नंदन ।   तेज प्रताप महा जगवंदन ॥
+    बिद्यावान गुनी अति चातुर ।   राम काज करिबे को आतुर ॥  प्रभु चरित्र सुनिबे को रसिया ।  राम लखन सीता मन बसिया ॥८   सूक्ष्म रूप धरि सियहिं दिखावा ।   बिकट रूप धरि लंक जरावा ॥
+    भीम रूप धरि असुर सँहारे ।  रामचन्द्र के काज सँवारे ॥ लाय सजीवन लखन जियाए ।    श्री रघुबीर हरषि उर लाये ॥ रघुपति कीन्ही बहुत बड़ाई ।   तुम मम प्रिय भरतहि सम भाई ॥१२
+    सहस बदन तुम्हरो जस गावैं ।    अस कहि श्रीपति कण्ठ लगावैं ॥    सनकादिक ब्रह्मादि मुनीसा ।   नारद सारद सहित अहीसा ॥  जम कुबेर दिगपाल जहाँ ते ।    कबि कोबिद कहि सके कहाँ ते ॥
+    तुम उपकार सुग्रीवहिं कीह्ना । राम मिलाय राज पद दीह्ना ॥१६ तुम्हरो मंत्र बिभीषण माना ।   लंकेश्वर भए सब जग जाना ॥    जुग सहस्त्र जोजन पर भानु ।    लील्यो ताहि मधुर फल जानू ॥
+    प्रभु मुद्रिका मेलि मुख माहीं । जलधि लाँघि गये अचरज नाहीं ॥  दुर्गम काज जगत के जेते ।    सुगम अनुग्रह तुम्हरे तेते ॥२०   राम दुआरे तुम रखवारे ।   होत न आज्ञा बिनु पैसारे ॥
+    सब सुख लहै तुम्हारी सरना । तुम रक्षक काहू को डरना ॥   आपन तेज सम्हारो आपै ।   तीनों लोक हाँक तै काँपै ॥    भूत पिशाच निकट नहिं आवै ।  महावीर जब नाम सुनावै ॥२४
+    नासै रोग हरै सब पीरा ।    जपत निरंतर हनुमत बीरा ॥  संकट तै हनुमान छुडावै ।  मन क्रम बचन ध्यान जो लावै ॥ सब पर राम तपस्वी राजा ।   तिनके काज सकल तुम साजा ॥
+    और मनोरथ जो कोई लावै ।   सोई अमित जीवन फल पावै ॥२८   चारों जुग परताप तुम्हारा ।  है परसिद्ध जगत उजियारा ॥    साधु सन्त के तुम रखवारे ।  असुर निकंदन राम दुलारे ॥
+    अष्ट सिद्धि नौ निधि के दाता । अस बर दीन जानकी माता ॥    राम रसायन तुम्हरे पासा ।   सदा रहो रघुपति के दासा ॥३२    तुम्हरे भजन राम को पावै ।  जनम जनम के दुख बिसरावै ॥
+    अंतकाल रघुवरपुर जाई ।    जहाँ जन्म हरिभक्त कहाई ॥  और देवता चित्त ना धरई ।    हनुमत सेइ सर्ब सुख करई ॥    संकट कटै मिटै सब पीरा ।   जो सुमिरै हनुमत बलबीरा ॥३६
+    जै जै जै हनुमान गोसाईं ।    कृपा करहु गुरुदेव की नाईं ॥  जो सत बार पाठ कर कोई ।    छूटहि बंदि महा सुख होई ॥   जो यह पढ़ै हनुमान चालीसा । होय सिद्धि साखी गौरीसा ॥
+    तुलसीदास सदा हरि चेरा ।    कीजै नाथ हृदय मह डेरा ॥४०
+
+॥ दोहा ॥
+पवन तनय संकट हरन,    मंगल मूरति रूप ।
+राम लखन सीता सहित,  हृदय बसहु सुर भूप ॥                                \/
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+#define pi 3.1415926535897932384626433832795
+#define mod 1000000007
+#define MOD 1000000007
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repr(i, k, n) for (int i = k; i >= n; --i)
+#define vl vector<ll>
+#define vi vector<int>
+#define print(ans) printf("%.9f\n", ans)
+#define all(x) (x).begin(), (x).end()
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+struct custom_hash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
+typedef unordered_map<ll, ll,custom_hash> mll; typedef unordered_set<ll,custom_hash> sll; typedef queue<ll> qll;typedef gp_hash_table<ll,ll,custom_hash> table;
+// --------->>> Debugging <<<-------
+void debug(ll t) { cerr << t; } void debug(int t) { cerr << t; }void debug(char t) { cerr << t; } void debug(string t) { cerr << t; } void debug(double t) { cerr << t; } void debug(ull t) { cerr << t; } void debug(ld t) { cerr << t; }
+
+template <class T, class V> void debug(pair<T, V> p);
+template <class T> void debug(vector<T> v);
+template <class T> void debug(set<T> v);
+template <class T, class V> void debug(map<T, V> v);
+template <class T> void debug(multiset<T> v);
+template <class T, class V> void debug(pair<T, V> p){cerr << "{";debug(p.first);cerr << ",";debug(p.second);cerr << "}";}
+template <class T>
+void debug(vector<T> v){cerr << "[ ";for (T v1 : v){debug(v1);cerr << " ";}cerr << "]";}
+template <class T>
+void debug(set<T> s){cerr << "[ ";for (T s1 : s){debug(s1);cerr << " ";}cerr << "]";}
+template <class T>
+void debug(multiset<T> s){cerr << "[ ";for (T s1 : s){debug(s1);cerr << " ";}cerr << "]";}
+template <class T, class V>void debug(map<T, V> m){cerr << "[ ";for (auto m1 : m){debug(m1);cerr << " ";}cerr << "]";}
+// ---------->>>  ###########  <<<---------- //
+
+namespace modop{ll madd(ll a, ll b){return (a + b) % mod;}ll msub(ll a, ll b){return (((a - b) % mod) + mod) % mod;}ll mmul(ll a, ll b){return ((a % mod) * (b % mod)) % mod;}ll mpow(ll base, ll exp){ll res = 1;while (exp){if (exp % 2 == 1){res = (res * base) % mod;}exp >>= 1;base = (base * base) % mod;}return res;}ll minv(ll base){    return mpow(base, mod - 2);}ll mdiv(ll a, ll b){    return mmul(a, minv(b));}    const ll FACTORIAL_SIZE = 1.1e6;    ll fact[FACTORIAL_SIZE], ifact[FACTORIAL_SIZE];    void gen_factorial(ll n)    {fact[0] = fact[1] = ifact[0] = ifact[1] = 1;for (ll i = 2; i <= n; i++){    fact[i] = (i * fact[i - 1]) % mod;}ifact[n] = minv(fact[n]);for (ll i = n - 1; i >= 2; i--){    ifact[i] = ((i + 1) * ifact[i + 1]) % mod;}}ll nck(ll n, ll k){if (k == 0){    return 1;}ll den = (ifact[k] * ifact[n - k]) % mod;return (den * fact[n]) % mod;}}
+long long expo(long long x, long long y){ long long res = 1;x = x % mod;while (y > 0){if (y & 1)    res = (res * x) % mod;y = y >> 1LL, x = (x * x) % mod;}return res;}
+
+void solve()
+{
+    
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    freopen("error.txt", "w", stderr);
+#endif
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ll testcase;
+    // testcase = 1;
+    cin >> testcase;
+
+    while (testcase--)
+    {
+        solve();
+        debug("\n");
+    }
+}
